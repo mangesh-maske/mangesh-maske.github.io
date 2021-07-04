820c17e25c1c4bb7194120e5b7a93d7390d28281
Mangesh Maske <mangesh.mask@gmail.com>
2021-07-04 16:38:11 +0530

    added new files

---
+++ precache-manifest.caee4b4b34ccf5fc0f984234fb1cc451.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "402681e7b69c0689753dfe4d82e7f9c8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "03fb110f3431ff46ee43",
+    "url": "/static/css/main.2335bf23.chunk.css"
+  },
+  {
+    "revision": "686849b111db4f8fc50b",
+    "url": "/static/js/2.5278f1f8.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.5278f1f8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "03fb110f3431ff46ee43",
+    "url": "/static/js/main.78c10490.chunk.js"
+  },
+  {
+    "revision": "7dcab7a4621ddd93a88a",
+    "url": "/static/js/runtime-main.50c526f6.js"
+  }
+]);
